a2b691be5935de103d1c9de4850f3aaa9cbaf18a
moha-alaraby <mimo38185@gmail.com>
2025-08-31 10:49:41 +0300

    Add files via upload

---
+++ main.js/3.js
@@ -0,0 +1,9 @@
+// menu
+let menu = document.querySelector(".links .click");
+let into = document.querySelector(".landing .into");
+console.log(menu)
+console.log(into)
+menu.onclick = ()=>{
+    into.classList.toggle("on")
+    console.log("mohamed")
+}
